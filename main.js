c713f560668e4101fb777afab025105a84461613
jpnm-panda <jpnm0415shkm@gmail.com>
2022-03-10 22:15:35 +0900

    コンポーネントをお試し

--- main.js
+++ main.js
@@ -1,27 +1,87 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-        brand: 'Vue Mastery',
-        product: 'Socks',
-        selectedVariant: 0,
-        link: 'https://www.amazon.com/s?k=socks&ref=nb_sb_noss',
-        onSale: true,
-        details: ["80% cotton", "20% polyester", "Gender-neutral"],
-        variants: [{
-                variantTd: 2234,
-                variantColor: "green",
-                variantImage: "./assets/vmSocks-green.jpg",
-                variantQuantity: 10
-            },
-            {
-                variantId: 2235,
-                variantColor: "blue",
-                variantImage: "./assets/vmSocks-blue.jpeg",
-                variantQuantity: 0
-            }
-        ],
-        sizes: ["S", "M", "L", "XL"],
-        cart: 0
+Vue.component('product-details', {
+    props: {
+        details: {
+            type: Array,
+            required: true
+        }
+    },
+    template: `
+    <ul>
+      <li v-for="detail in details">{{ detail }}</li>
+    </ul>
+    `
+})
+
+Vue.component('product', {
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
+    },
+    template: `
+    <div class="product">
+
+    <div class="product-image">
+        <img v-bind:src="image">
+    </div>
+
+    <div class="product-info">
+        <h1>{{ title }}</h1>
+        <p v-if="inStock">In Stock</p>
+        <p v-else :class="{ outOfStock: !inStock }">Out of Stock</p>
+        <p>Shipping: {{ shipping }}</p>
+        <a :href="link" target="_blank">Show more Socks info</a>
+        <br>
+        <p>{{ sale }}</p>
+
+        <product-details :details="details"></product-details>
+
+        <ul>
+            <li v-for="size in sizes">{{ size }}</li>
+        </ul>
+
+        <div v-for="(variant, index) in variants" :key="variant.variantId" class="color-box"
+            :style="{ backgroundColor: variant.variantColor}" @mouseover="updateProduct(index)">
+        </div>
+
+        <button v-on:click="addToCart" :disabled="!inStock" :class="{ disabledButton: !inStock }">Add to
+            Cart</button>
+
+        <button @click="removeFromCart">Remove From Cart</button>
+
+        <div class="cart">
+            <p>Cart({{ cart }})</p>
+        </div>
+
+    </div>
+
+</div>
+    `,
+    data() {
+        return {
+            brand: 'Vue Mastery',
+            product: 'Socks',
+            selectedVariant: 0,
+            link: 'https://www.amazon.com/s?k=socks&ref=nb_sb_noss',
+            onSale: true,
+            details: ["80% cotton", "20% polyester", "Gender-neutral"],
+            variants: [{
+                    variantTd: 2234,
+                    variantColor: "green",
+                    variantImage: "./assets/vmSocks-green.jpg",
+                    variantQuantity: 10
+                },
+                {
+                    variantId: 2235,
+                    variantColor: "blue",
+                    variantImage: "./assets/vmSocks-blue.jpeg",
+                    variantQuantity: 0
+                }
+            ],
+            sizes: ["S", "M", "L", "XL"],
+            cart: 0
+        }
     },
     methods: {
         addToCart() {
@@ -51,6 +111,20 @@ var app = new Vue({
                 return this.brand + '' + this.product + 'are on sale!'
             }
             return this.brand + '' + this.product + 'are not on sale!'
+        },
+        shipping() {
+            if (this.premium) {
+                return "Free"
+            }
+            return 2.99
         }
     },
+})
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        premium: true
+    }
+
 })
