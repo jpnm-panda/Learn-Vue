84c1e9882437478e39eb5d66cdba0fb4a197868e
jpnm-panda <jpnm0415shkm@gmail.com>
2022-03-12 16:11:35 +0900

    formを作成してみる

--- main.js
+++ main.js
@@ -12,6 +12,84 @@ Vue.component('product-details', {
     `
 })
 
+Vue.component('product-review', {
+    template: `
+        <form class="review-form" @submit.prevent="onSubmit">
+
+            <p v-if="errors.length">
+                <b>Please correct the following error(s):</b>
+                <ul>
+                    <li v-for="error in errors">{{ error}}</li>
+                </ul>
+            </p>
+            <p>
+                <label for="name">Name:</label>
+                <input id="name" v-model="name" placeholder="name" >
+            </p>
+
+            <p>
+                 <label for="review">Review:</label>
+                 <textarea id="review" v-model="review"></textarea>
+            </p>
+
+            <p>
+                <label for="rating">Rating:</label>
+                <select id="rating" v-model.number="rating">
+                    <option>5</option>
+                    <option>4</option>
+                    <option>3</option>
+                    <option>2</option>
+                    <option>1</option>
+                </select>
+            </p>
+
+            <p>Would you recommend this product?</p>
+                <label>Yes
+                    <input type="radio" value=Yes v-model="recommend">
+                </label>
+                <label>No
+                    <input type="radio" value="No" v-model="recommend"">
+                </label>
+            <p>
+                <input type="submit" value="Submit">
+            </p>
+        </form>
+    `,
+    data() {
+        return {
+            name: null,
+            review: null,
+            rating: null,
+            recommend: null,
+            errors: []
+        }
+    },
+    methods: {
+        onSubmit() {
+            if (this.name && this.review && this.rating) {
+                let productReview = {
+                    name: this.name,
+                    review: this.review,
+                    rating: this.rating,
+                    recommend: this.recommend
+                }
+                this.$emit('review-submitted', productReview)
+                this.name = null
+                this.review = null
+                this.rating = null
+                this.recommend = null
+            } else {
+                if (!this.name) this.errors.push("Name required")
+                if (!this.review) this.errors.push("Review required")
+                if (!this.rating) this.errors.push("Rating required")
+                if (!this.recommend) this.error.push("Recommend required")
+            }
+
+
+        }
+    }
+})
+
 Vue.component('product', {
     props: {
         premium: {
@@ -49,11 +127,22 @@ Vue.component('product', {
             Cart</button>
 
         <button @click="removeFromCart">Remove From Cart</button>
-
-        
-
     </div>
 
+    <div>
+        <h2>Reviews</h2>
+        
+        <p v-if="!reviews.length">There are no reviews yet.</p>
+        <ul>
+            <li v-for="review in reviews">
+                <p>{{ review.name }}</p>
+                <p>Rating: {{ review.rating }}</p>
+                <p>{{ review.review }}</p>
+                <P>Would you recommend this product?: {{ review.recommend }}</P>
+            </li>
+        </ul>
+    </div>
+    <product-review @review-submitted="addReview"></product-review>
 </div>
     `,
     data() {
@@ -78,7 +167,8 @@ Vue.component('product', {
                 }
             ],
             sizes: ["S", "M", "L", "XL"],
-            cart: 0
+            cart: 0,
+            reviews: []
         }
     },
     methods: {
@@ -92,6 +182,9 @@ Vue.component('product', {
         updateProduct(index) {
             this.selectedVariant = index
             console.log(index)
+        },
+        addReview(productReview) {
+            this.reviews.push(productReview)
         }
     },
     computed: {
@@ -119,6 +212,8 @@ Vue.component('product', {
         }
     },
 })
+
+
 
 var app = new Vue({
     el: '#app',
